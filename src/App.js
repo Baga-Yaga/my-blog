5258e66d31df19f1b9ba0b1068083dfb1aac14af
python_practicals <naikamar1029@gmail.com>
2024-10-18 18:16:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// // src/App.js
+// import React from 'react';
+// import LandingPage from './LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const App = () => {
+//     return (
+//         <div>
+//             <LandingPage />
+//         </div>
+//     );
+// };
+
+// export default App;
+
+// src/App.js
+import React from 'react';
+import './App.css'; // Existing styles
+import './LandingPage.css'; // Import your LandingPage styles
+import LandingPage from './LandingPage'; // Import the LandingPage component
+
+const App = () => {
+    return (
+        <div className="App">
+            <LandingPage />
+        </div>
+    );
+};
 
 export default App;
